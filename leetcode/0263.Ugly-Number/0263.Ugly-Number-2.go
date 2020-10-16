dbbf79e6bc1e620ce836a8d7b050ff09589cdb46
Scorpiosu <aa891119@gmail.com>
2020-10-16 23:10:36 +0800

    2020-10-16-commit

---
+++ leetcode/0263.Ugly-Number/0263.Ugly-Number-2.go
@@ -0,0 +1,15 @@
+package leetcode
+
+func isUgly2(num int) bool {
+	if num == 1 {
+		return true
+	}
+	if num <= 0 {
+		return false
+	}
+	for _, i := range [3]int{2, 3, 5} {
+		for ; num%i == 0 && num > 0; num /= i {
+		}
+	}
+	return num == 1
+}
